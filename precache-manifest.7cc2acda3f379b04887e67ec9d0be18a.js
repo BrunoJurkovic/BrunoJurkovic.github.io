bfe1fab4d810641bd88632ddc3869d57a8b1c74c
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-04-15 21:23:36 +0200

    Updates

---
+++ precache-manifest.7cc2acda3f379b04887e67ec9d0be18a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcccf83d757579cbf8a9d26fdf79b3f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b7da2482b8a4c062b92",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "07d30b992fca8ebc2dc4",
+    "url": "./static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "4b7da2482b8a4c062b92",
+    "url": "./static/js/2.d8d8be5a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.d8d8be5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d30b992fca8ebc2dc4",
+    "url": "./static/js/main.bd8f3796.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
